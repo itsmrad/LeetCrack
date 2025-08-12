f8fb36123d19071d69e37f471ab280fa7ad51401
Anupam <itsmrad@gmail.com>
2025-08-12 21:49:53 +0530

    replace Hello component with CredentialForm in App component

--- src/entrypoints/popup/App.tsx
+++ src/entrypoints/popup/App.tsx
@@ -1,16 +1,12 @@
 import { useState } from 'react';
-import reactLogo from '@/assets/react.svg';
-import wxtLogo from '/wxt.svg';
-import './App.css';
-import Hello from './sections/Hello';
+import CredentialForm from './components/CredentialForm';
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
       <div className=''>
-        <Hello />
+        <CredentialForm />
       </div>
     </>
   );
